fdd0446b3e3d72b6f033ce35f32855bc8910b81d
Liaoyuqing <406795358@qq.com>
2018-11-23 20:29:29 +0800

    提交电商架构第一次

---
+++ pinyougou/pinyougou-dao/src/main/java/com/pinyougou/mapper/BrandMapper.java
@@ -0,0 +1,13 @@
+package com.pinyougou.mapper;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * Date:2018/11/23
+ * Author:YQ.Liao
+ */
+public interface BrandMapper {
+    public List<TbBrand>queryAll();
+}
---
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * Date:2018/11/23
+ * Author:YQ.Liao
+ */
+public interface BrandService {
+    public List<TbBrand>queryAll();
+}
